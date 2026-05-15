3f437e5299c324106a21b73bcc17245d2575d444
Dhruv Verma <vermadhruv09112002@gmail.com>
2026-05-15 16:43:33 +0530

    fix dist docker ignore

--- construction-frontend/src/lib/index.js
+++ construction-frontend/src/lib/index.js
@@ -1,4 +1,4 @@
 export const API_URL =
   process.env.NODE_ENV === "production"
-    ? "https://inoside.onrender.com/api"
+    ? "https://buildcon-api.rippotaiarchitecture.com/api"
     : "http://localhost:5000/api";
