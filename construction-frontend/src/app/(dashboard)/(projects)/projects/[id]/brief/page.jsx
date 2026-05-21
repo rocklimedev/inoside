cf65b410b60bcdc68e19959b5475eeaaea590c29
Dhruvermafz <vermadhruv09112002@gmail.com>
2026-05-21 08:10:34 +0530

    asdf

--- construction-frontend/src/app/(dashboard)/(projects)/projects/[id]/brief/page.jsx
+++ construction-frontend/src/app/(dashboard)/(projects)/projects/[id]/brief/page.jsx
@@ -1,9 +1,9 @@
-// page.js
-
 export const dynamic = "force-dynamic";
 
 import BriefPageClient from "./BriefPageClient";
 
-export default function Page({ params }) {
-  return <BriefPageClient projectId={params.id} />;
+export default async function Page({ params }) {
+  const { id } = await params;
+
+  return <BriefPageClient projectId={id} />;
 }
